99c61efbae1edd3cde5e3919d409d8286e7a5853
Sergey Zybko <zyb429@gmail.com>
2023-07-13 18:32:06 +0300

    Title card beta

--- src/components/content.js
+++ src/components/content.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "../App.css";
-
+import PreviewImage from "../icons/preview.jpg";
 function Content () {
     return (
         <div className={"Content"}>
-            ada
+            <div className={"TitleCard"}>
+                <img src={PreviewImage} alt={"preview"} className={"PreviewImg"}></img>
+                <div className={"TitleDescription"}>
+                    <h1 className={"TitleName"}>Blue Lock</h1>
+                    <p>Тип: </p>
+                </div>
+            </div>
+
         </div>
     );
 }
